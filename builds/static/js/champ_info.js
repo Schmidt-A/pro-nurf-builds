05e1954b7782fb287c0bd079055a0c6690170197
Schmidt-A <caffeinated.tweek@gmail.com>
2015-04-16 17:47:52 +0000

    Animations, anchors.

--- builds/static/js/champ_info.js
+++ builds/static/js/champ_info.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
 	$("#overview img").tooltip({'placement': 'left'});
 	$("#overview .panel-heading").tooltip({'placement': 'left'});
 	$(".progress").tooltip({'placement': 'left'});
+
+	$('html, body').animate({
+		scrollTop: $('#anchor').offset().top
+	}, 800);
+
+	$('.progress-bar').each(function() {
+		var percent = $(this).attr('value')
+		$(this).animate({
+			width: percent
+		}, 2000);
+	});
+
+
 });
