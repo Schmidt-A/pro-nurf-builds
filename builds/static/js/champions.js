329dd07fc3393b15725613750f9a7d19dff26c18
Schmidt-A <caffeinated.tweek@gmail.com>
2015-04-16 22:30:27 +0000

    Animated number counter.

---
+++ builds/static/js/champions.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+        var end = $('#counter').attr('value')
+        $({someValue: 0}).animate({someValue: end}, {
+                duration: 2000,
+                easing:'swing', // can be anything
+                step: function() { // called on every step
+                        // Update the element's text with rounded-up value:
+                        $('#counter').text(Math.round(this.someValue));
+                }
+        });
+});
