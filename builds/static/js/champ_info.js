ab0b105d6f6331548c83d669041364d1c9e3bf13
Schmidt-A <caffeinated.tweek@gmail.com>
2015-04-17 21:42:56 +0000

    Tooltip hovers on items.

--- builds/static/js/champ_info.js
+++ builds/static/js/champ_info.js
@@ -1,16 +1,69 @@
 $(document).ready(function(){
-	var animated = false;
+        // ******************
+        // Tooltip setup
+        // ******************
+        item_tooltip_html = [
+                '<div class="tooltip item_tooltip" role="tooltip">',
+                '<div class="tooltip-arrow"></div>',
+                '<div class="tooltip-inner"></div>',
+                '</div>'].join('');
 
-	$("#overview img").tooltip({'placement': 'left'});
+        $("#urf-tab-div .item_icon").tooltip({
+            'placement': 'left', 
+            'container': 'body',
+            'html': true,
+            'template': item_tooltip_html
+        });
+
+	$("#overview .build_icon").tooltip({'placement': 'left', 'container': 'body'});
 	$("#overview .panel-heading").tooltip({'placement': 'left'});
 	$(".progress").tooltip({'placement': 'left'});
 
+        $('#urf-tab-div .item_icon').mouseover(function() {
+                console.log('test')
+                img = $(this).attr('src')
+                name = $(this).attr('i_name')
+                desc = $(this).attr('i_desc')
+                console.log(desc)
+
+                content = [
+                        '<div class="row">',
+                        '<div class="col-md-3">',
+                        '<img src="' + img + '"></img>',
+                        '</div>',
+                        '<div class="col-md-9">',
+                        '<span><b>' + name + '</b><br>' + desc + '</span>',
+                        '</div>',
+                        '</div>'
+                ].join('');
+
+                $(this).attr('data-original-title', content);
+                $(this).tooltip('fixTitle').tooltip('show');
+
+        });
+
+        // ******************
+
+        // Move screen down to Champion title
 	$('html, body').animate({
 		scrollTop: $('#anchor').offset().top
 	}, 800);
 
+        // Animate champ stats bars
+	$('.progress-bar-champ').each(function() {
+		var percent = $(this).attr('value')
+		$(this).animate({
+			width: percent
+		}, 2000);
+	});
 
+
+        // ******************
+        // Item breakdown tab
+        // ******************
+	animated = false;
 	table = $('.table').stupidtable();
+        table.find('thead th').eq(1).stupidsort();
 
 	table.on("aftertablesort", function (event, data) {
 		var th = $(this).find("th");
@@ -18,15 +71,6 @@ $(document).ready(function(){
 		var dir = $.fn.stupidtable.dir;
 		var arrow = data.direction === dir.ASC ? "&uarr;" : "&darr;";
 		th.eq(data.column).append('<span class="arrow">' + arrow +'</span>');
-	});
-
-        table.find('thead th').eq(1).stupidsort();
-
-	$('.progress-bar-champ').each(function() {
-		var percent = $(this).attr('value')
-		$(this).animate({
-			width: percent
-		}, 2000);
 	});
 
 	$('#items_tab').click(function(){
@@ -40,5 +84,6 @@ $(document).ready(function(){
 			});
 		}
 	});
+        // ******************
 				
 });
